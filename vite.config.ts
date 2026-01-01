149ebba14e8c149b2922a195a63f258f9ea15a6d
Owolabi shinaayomi <100287793+koredeowoblow@users.noreply.github.com>
2026-01-01 02:14:42 +0100

    Add allowedHosts to Vite server config
    Configured the Vite dev server to restrict access to specified hosts, including 'eschool-frontend.onrender.com' and all '.onrender.com' domains, for improved security and deployment compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
+      allowedHosts: [
+        'eschool-frontend.onrender.com',
+         '.onrender.com'
+      ],
       proxy: {
         '/api/v1/broadcasting': {
           target: 'https://eschool-1.onrender.com', // Production backend
